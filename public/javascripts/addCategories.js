affc571a80582a4de3ec22fdbe46377072df807c
unknown <daniellewiskelly@gmail.com>
2019-03-14 15:28:45 +0000

    Got images uploading to site

--- public/javascripts/addCategories.js
+++ public/javascripts/addCategories.js
@@ -12,21 +12,26 @@ fetch(url)
     var img;
     
     for(var i = 0; i < n; i++) {
-        nameNoSpaces = jsonResponse.categories[i].name.split(' ').join('_').replace(/,/g, '');
-        divID = "square_" + nameNoSpaces;
+        nameFormatted = jsonResponse.categories[i].name.split(' ').join('_').replace(/,/g, '').toLowerCase().replace(/&_/, '');
+        divID = "square_" + nameFormatted;
 
-        console.log("divID: " + divID);
         document.body.innerHTML+='<div class="gridsquare" id=' + divID + ' ">'
         + (j++) + jsonResponse.categories[i].name
          + '</div>';
 
         img = document.createElement("img");
-        img.src = jsonResponse.categories[i].image;
+        img.src = jsonResponse.categories[i].image.replace(/public/, '');
 
-        console.log("image source: " + img.src);
         src = document.getElementById(divID);
-        console.log(src);
         src.appendChild(img);
+
+        console.log("divID: " + divID);
+        console.log("image source: " + img.src);
+        console.log("placing source: " + src);
+        console.log("\n");
+
+
+
      }  
   });
 
