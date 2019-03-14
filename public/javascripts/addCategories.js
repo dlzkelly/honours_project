1b7b502c9c861add0b71960d01a3df9c397676a0
unknown <daniellewiskelly@gmail.com>
2019-03-14 12:56:04 +0000

    Working on reading in json object into html

--- public/javascripts/addCategories.js
+++ public/javascripts/addCategories.js
@@ -1,8 +1,43 @@
-let url = '';
-
+url = "https://api.myjson.com/bins/19esd2";
 fetch(url)
-.then(res => res.json())
-.then((out) => {
-  console.log('Checkout this JSON! ', out);
-})
-.catch(err => { throw err });
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    var n = jsonResponse.categories.length; 
+    
+    console.log(jsonResponse.categories.length);
+
+    document.getElementById("categorySection").innerHTML = "Hello";
+
+    var j=1;
+
+    n = 5;
+    for(var i = 1; i < n; i++) {
+        document.getElementById("categorySection").innerHtml+='<div class="row">';
+        console.log("we made it" + n);
+        for(k = 0; k < n; k++) {
+            document.getElementById("categorySection").innerHtml+='<div class="gridsquare">' + (j++) + '</div>';
+        }
+    
+        document.getElementById("categorySection").innerHtml+='</div>';
+     }  
+  });
+
+//   var n=4;//take grid column value as you want
+// var j=1;
+
+// for(var i = 1; i < n; i++) {
+//     document.body.innerHTML+='<div class="row">';
+
+//     for(k = 0; k < n; k++) {
+//         document.body.innerHTML+='<div class="gridsquare">' + (j++) + '</div>';
+//     }
+
+//     document.body.innerHTML+='</div>';
+// }
+
+
+
+
+
