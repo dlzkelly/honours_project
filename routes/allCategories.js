567995263fc815164cb9f20be0e28332c33cbd66
unknown <daniellewiskelly@gmail.com>
2019-03-15 17:15:06 +0000

    subcategories working

--- routes/allCategories.js
+++ routes/allCategories.js
@@ -7,4 +7,16 @@ router.get('/', function(req, res, next) {
     res.render('allCategories', { title: 'All Categories', dataSet});
 });
 
+router.get('/:category', function(req, res, next) { // https://expressjs.com/en/api.html#req.params
+    console.log(req.params);
+    var category = dataSet.categories.find(function(current)
+    {
+        if(current.name.split(' ').join('_').replace(/,/g, '').toLowerCase().replace(/&_/, '') == req.params.category)
+        {
+            return true;
+        }
+    })
+    console.log(category);
+    res.render("subcategory", {category})
+});
 module.exports = router;
