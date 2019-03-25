82e8c4c28cccae7a4d3809e9986ddda77c1c037b
unknown <daniellewiskelly@gmail.com>
2019-03-25 16:49:36 +0000

    breadcrumb added, products, linked up, css formatted

--- routes/allCategories.js
+++ routes/allCategories.js
@@ -20,30 +20,34 @@ router.get('/:category', function(req, res, next) { // https://expressjs.com/en/
     res.render("subcategory", {category})
 });
 
+//NEED TO PASS IN FRANKENSTEIN PAGE NAME SO THAT THE BREADRUMB CAN LINK TO THE PRODUCT'S SUBCATEGORY
 router.get('/cameras_photography/telescopes_binoculars', function(req, res, next) {
-    res.render('productBinoculars');
+    res.render('productBinoculars', {category: "Cameras & Photography", subcategory: "Telescopes & Binocolulars"});
 }); 
 router.get('/computers_tablets_networking/printers_scanners_supplies', function(req, res, next) {
-    res.render('productScanner');
+    res.render('productScanner', {category: "Computers, Tablets & Networking", subcategory: "Printers, Scanners & Supplies"});
 });
 router.get('/pet_supplies/birds', function(req, res, next) {
-    res.render('productCage');
+    res.render('productCage' , {category: "Pet Supplies", subcategory: "Birds"});
 });
 router.get('/music/cassettes', function(req, res, next) {
-    res.render('productCassette');
+    res.render('productCassette', {category: "Music", subcategory: "Cassettes"});
 });
 router.get('/toys_games/jigsaws_puzzles', function(req, res, next) {
-    res.render('productJigsaw');
+    res.render('productJigsaw', {category: "Toys & Games", subcategory: "Jigsaws & Puzzles"});
 });
 router.get('/sporting_goods/scooters', function(req, res, next) {
-    res.render('productScooter');
+    res.render('productScooter', {category: "Sporting Goods", subcategory: "Scooters"});
 });
 router.get('/mobile_phones_communication/smart_watches', function(req, res, next) {
-    res.render('productSmartwatch');
+    res.render('productSmartwatch', {category: "Mobile Phones & Communication", subcategory: "Smart Watches"});
 });
-router.get('/health_beauty/e-cigarettes_vapes_accs', function(req, res, next) {
-    res.render('productVape');
+router.get('/health_beauty/e_cigarettes_vapes_accs', function(req, res, next) {
+    res.render('productVape', {category: "Health & Beauty", subcategory: "E-Cigarettes & Vapes"});
 });
 
+router.get('/:cat/:sub', function(req, res, next) {
+    res.render('noProducts', {category: req.params.cat , subcategory: req.params.sub});
+});
 
 module.exports = router;
