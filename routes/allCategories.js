980025c395d90d159231036d85ba59e55ed6a07c
unknown <daniellewiskelly@gmail.com>
2019-03-25 17:52:47 +0000

    Trouble shooting and tidying up breadcrum

--- routes/allCategories.js
+++ routes/allCategories.js
@@ -22,28 +22,28 @@ router.get('/:category', function(req, res, next) { // https://expressjs.com/en/
 
 //NEED TO PASS IN FRANKENSTEIN PAGE NAME SO THAT THE BREADRUMB CAN LINK TO THE PRODUCT'S SUBCATEGORY
 router.get('/cameras_photography/telescopes_binoculars', function(req, res, next) {
-    res.render('productBinoculars', {category: "Cameras & Photography", subcategory: "Telescopes & Binocolulars"});
+    res.render('productBinoculars', {category: "Cameras & Photography", subcategory: "Telescopes & Binocolulars", catFormatted: "", subFormatted: ""});
 }); 
 router.get('/computers_tablets_networking/printers_scanners_supplies', function(req, res, next) {
-    res.render('productScanner', {category: "Computers, Tablets & Networking", subcategory: "Printers, Scanners & Supplies"});
+    res.render('productScanner', {category: "Computers, Tablets & Networking", subcategory: "Printers, Scanners & Supplies", catFormatted: "", subFormatted: ""});
 });
 router.get('/pet_supplies/birds', function(req, res, next) {
-    res.render('productCage' , {category: "Pet Supplies", subcategory: "Birds"});
+    res.render('productCage' , {category: "Pet Supplies", subcategory: "Birds", catFormatted: "", subFormatted: ""});
 });
 router.get('/music/cassettes', function(req, res, next) {
-    res.render('productCassette', {category: "Music", subcategory: "Cassettes"});
+    res.render('productCassette', {category: "Music", subcategory: "Cassettes", catFormatted: "", subFormatted: ""});
 });
 router.get('/toys_games/jigsaws_puzzles', function(req, res, next) {
-    res.render('productJigsaw', {category: "Toys & Games", subcategory: "Jigsaws & Puzzles"});
+    res.render('productJigsaw', {category: "Toys & Games", subcategory: "Jigsaws & Puzzles", catFormatted: "", subFormatted: ""});
 });
 router.get('/sporting_goods/scooters', function(req, res, next) {
-    res.render('productScooter', {category: "Sporting Goods", subcategory: "Scooters"});
+    res.render('productScooter', {category: "Sporting Goods", subcategory: "Scooters", catFormatted: "", subFormatted: ""});
 });
 router.get('/mobile_phones_communication/smart_watches', function(req, res, next) {
-    res.render('productSmartwatch', {category: "Mobile Phones & Communication", subcategory: "Smart Watches"});
+    res.render('productSmartwatch', {category: "Mobile Phones & Communication", subcategory: "Smart Watches", catFormatted: "", subFormatted: ""});
 });
 router.get('/health_beauty/e_cigarettes_vapes_accs', function(req, res, next) {
-    res.render('productVape', {category: "Health & Beauty", subcategory: "E-Cigarettes & Vapes"});
+    res.render('productVape', {category: "Health & Beauty", subcategory: "E-Cigarettes & Vapes", catFormatted: "", subFormatted: ""});
 });
 
 router.get('/:cat/:sub', function(req, res, next) {
