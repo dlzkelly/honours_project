cd6fd855ae94548b423020de840fd67dc226a351
unknown <daniellewiskelly@gmail.com>
2019-03-26 22:54:26 +0000

    index working correctly

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var dataSet = require('../dataSet.json');
+
+/* GET All Categories page. */
+router.get('/', function(req, res, next) {
+    res.render('allCategories', { title: 'All Categories', dataSet});
+});
+module.exports = router;
